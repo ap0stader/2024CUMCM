01067e9836d3e315fb48b3fc05c698fcb2a29ffd
ap0stader <ap0stader@outlook.com>
2024-09-07 22:23:36 +0800

    开始Q5

---
+++ Q5_generation_forward.py
@@ -0,0 +1,78 @@
+import os
+import pickle
+
+import numpy as np
+
+import CONST
+import Q4
+from UTIL import calc_speed
+
+# 创建结果保存目录
+RESULT_DIR = CONST.RESULT_ROOT + "Q5_generation_forward/"
+os.makedirs(RESULT_DIR, exist_ok=True)
+
+# 精细模拟，一秒钟均分成几份
+SEC_DIVISION = 50
+# 模拟的秒数
+SIM_SECOND = 100
+# 模拟的轮数
+SIM_STEP = SIM_SECOND * SEC_DIVISION
+
+# 每一秒龙头前把手行进的弧长
+HEAD_SECOND_CURVE_LENGTH = CONST.DEFAULT_HEAD_SPEED / SEC_DIVISION
+
+# 确定起始的位置
+head_theta = [Q4.ThetaType.ROUND_IN, Q4.theta_C1_start]
+
+# 数据存储
+data = list()
+
+
+def store_point(index, theta, speed):
+    data[index].append([theta, Q4.theta2x(theta), Q4.theta2y(theta), speed])
+
+
+for step in range(SIM_STEP + 1):
+    # 因为从0秒开始模拟，故sec就是数组下标
+    data.append(list())
+    print("正在计算第 " + "{:.4f}".format(step / SEC_DIVISION) + " 秒信息")
+    print("当前龙头前把手的位置θ类型为" + str(head_theta[0]) +
+          "，值为" + "{:.4f}".format(head_theta[1] / (2 * np.pi)) + "x2π")
+    # 绘制龙头前把手的位置
+    store_point(step, head_theta, CONST.DEFAULT_HEAD_SPEED)
+    # 求解第一节龙身前把手的位置
+    first_body_theta = Q4.calc_next_handle_theta(head_theta, CONST.HEAD_BENCH_LEN)
+    first_body_speed = calc_speed(Q4.get_shape(head_theta), CONST.DEFAULT_HEAD_SPEED, head_theta[1],
+                                  Q4.get_shape(first_body_theta), first_body_theta[1])
+    # 存储第一节龙身前把手的信息
+    store_point(step, first_body_theta, first_body_speed)
+
+    # 第2节龙身前把手至第222节龙身前把手的位置
+    last_body_theta = first_body_theta
+    last_body_speed = first_body_speed
+    for ben in range(2, CONST.BODY_TAIL_COUNT + 1):
+        # 求解下一节龙身前把手的位置和速度
+        next_body_theta = Q4.calc_next_handle_theta(last_body_theta, CONST.OTHER_BENCH_LEN)
+        next_body_speed = calc_speed(Q4.get_shape(last_body_theta), last_body_speed, last_body_theta[1],
+                                     Q4.get_shape(next_body_theta), next_body_theta[1])
+        # 存储这一节龙身前把手的信息
+        store_point(step, next_body_theta, next_body_speed)
+        # 迭代
+        last_body_theta = next_body_theta
+        last_body_speed = next_body_speed
+
+    # 求解龙头前把手新的位置
+    head_theta = Q4.calc_next_head_theta(head_theta, HEAD_SECOND_CURVE_LENGTH)
+
+    print("第一个点的情况：" + str(data[step][0]))
+    print("第二个点的情况：" + str(data[step][1]))
+    print("最后一个点的情况：" + str(data[step][223]))
+
+# 数据解读方法
+# 第一维为模拟时间
+# 第二维为把手，0表示龙头前把手，1-222表示221节龙身+1节龙尾前把手，223表示龙尾后把手
+# 第三维，第一个数字是theta，第二个数字是x坐标，第三个数字是y坐标，第四个数字是速度
+# theta的第一维为类型，第二维维值，单位为弧度，其余数字的长度单位均为cm
+
+with open(RESULT_DIR + 'data.pkl', 'wb') as f:
+    pickle.dump(data, f)
